20ca736472e76e4a40d8f7d6980278d43888dad6
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-05-12 11:40:50 +0200

    feat(ui): info etab interaction

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -20,18 +20,70 @@ import './assets/scss/main.scss'
 
 register()
 
+// Bottom sheet info
+const bottomSheetInfo = document.querySelector<HTMLElement>('#bottom-sheet-info')
+const bottomSheetInfoDraggable = bottomSheetInfo?.querySelector<HTMLElement>('.dragable')
+
+let isBottomSheetInfo = false
+
+function changeBottomSheetInfoState(show: boolean): void {
+  if (!bottomSheetInfo)
+    return
+  document.documentElement.style.overflowY = show ? 'hidden' : ''
+  bottomSheetInfo.style.display = show ? '' : 'none'
+}
+
+bottomSheetInfoDraggable?.addEventListener('click', () => {
+  isBottomSheetInfo = !isBottomSheetInfo
+  changeBottomSheetInfoState(isBottomSheetInfo)
+})
+
 // Eyebrow
 const eyebrow = document.querySelector<HTMLElement>('.eyebrow')
 const eyebrowButton = eyebrow?.querySelector<HTMLButtonElement>('.eyebrow-button')
 const eyebrowMenu = eyebrow?.querySelector<HTMLElement>('#eyebrow-menu')
-if (eyebrow && eyebrowButton && eyebrowMenu) {
-  let isEyebrowExpended = false
-  eyebrowButton.addEventListener('click', () => {
-    isEyebrowExpended = !isEyebrowExpended
-    eyebrowButton.ariaExpanded = isEyebrowExpended.toString()
-    const buttonIcon = eyebrowButton.querySelector<HTMLElement>('svg')
-    buttonIcon?.classList.remove('fa-chevron-down', 'fa-chevron-up')
-    buttonIcon?.classList.add(isEyebrowExpended ? 'fa-chevron-up' : 'fa-chevron-down')
-    eyebrowMenu.style.display = isEyebrowExpended ? '' : 'none'
-  })
+
+let isEyebrowExpended = false
+
+function changeEyebrowState(isExpended: boolean): void {
+  if (!eyebrow || !eyebrowButton || !eyebrowMenu)
+    return
+  eyebrowButton.ariaExpanded = isExpended.toString()
+  const buttonIcon = eyebrowButton.querySelector<HTMLElement>('svg')
+  buttonIcon?.classList.remove('fa-chevron-down', 'fa-chevron-up')
+  buttonIcon?.classList.add(isExpended ? 'fa-chevron-up' : 'fa-chevron-down')
+  eyebrowMenu.style.display = isExpended ? '' : 'none'
 }
+
+eyebrowButton?.addEventListener('click', () => {
+  isEyebrowExpended = !isEyebrowExpended
+  changeEyebrowState(isEyebrowExpended)
+})
+
+eyebrow?.querySelector<HTMLButtonElement>('#info-etab')?.addEventListener('click', () => {
+  isEyebrowExpended = !isEyebrowExpended
+  changeEyebrowState(isEyebrowExpended)
+  isBottomSheetInfo = !isBottomSheetInfo
+  changeBottomSheetInfoState(isBottomSheetInfo)
+})
+
+// Dropdown info
+const dropdownInfo = document.querySelector<HTMLElement>('.dropdown-info')
+const dropdownInfoButton = dropdownInfo?.querySelector<HTMLButtonElement>('.dropdown-info-button')
+const dropdownInfoMask = dropdownInfo?.querySelector<HTMLElement>('.dropdown-info-mask')
+const dropdownInfoMenu = dropdownInfo?.querySelector<HTMLElement>('#dropdown-info-menu')
+
+let isDropdownInfoExpended = false
+
+function changeDropdownInfoState(isExpended: boolean): void {
+  if (!dropdownInfo || !dropdownInfoButton || !dropdownInfoMask || !dropdownInfoMenu)
+    return
+  dropdownInfoButton.ariaExpanded = isExpended.toString()
+  isExpended ? dropdownInfoButton.classList.add('active') : dropdownInfoButton.classList.remove('active')
+  dropdownInfoMask.style.display = isExpended ? '' : 'none'
+  dropdownInfoMenu.style.display = isExpended ? '' : 'none'
+}
+dropdownInfoButton?.addEventListener('click', () => {
+  isDropdownInfoExpended = !isDropdownInfoExpended
+  changeDropdownInfoState(isDropdownInfoExpended)
+})
