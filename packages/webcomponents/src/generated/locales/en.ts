9d18c0f8a90e77b43ec7a3a298011df50424992a
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-05-21 12:22:19 +0200

    fix(eyebrow): logout translation

--- packages/webcomponents/src/generated/locales/en.ts
+++ packages/webcomponents/src/generated/locales/en.ts
@@ -21,7 +21,7 @@
 'sb1ae6414f46b7471': `My profile`,
 'sbddc966a2760f55c': `Change school`,
 'sbf71c25c7ca379d8': `School info`,
-'sc3a31f700d8e22a1': `Lougout`,
+'sc3a31f700d8e22a1': `Logout`,
 'sca0972101e448a98': `Staff
 (agricultural education)`,
 'se6226bfe4a365f9b': `Staff
