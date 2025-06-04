75753183052e5afa6c7657c65c4646a85b16f0d4
Ewann ROUX <ewann.roux@recia.fr>
2025-06-04 14:02:14 +0200

    feat(wc): make widgets-wrapper get values from adapter (WIP)

--- packages/webcomponents/widgets-wrapper/src/index.ts
+++ packages/webcomponents/widgets-wrapper/src/index.ts
@@ -17,12 +17,17 @@
 import type { TemplateResult } from 'lit'
 import { localized, updateWhenLocaleChanges } from '@lit/localize'
 import { css, html, LitElement, unsafeCSS } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { customElement, property, state } from 'lit/decorators.js'
+import { repeat } from 'lit/directives/repeat.js'
 import { componentName } from '../../common/config.ts'
 import { name } from '../package.json'
+import { Item } from './classes/Item.ts'
+import { WidgetData } from './classes/WidgetData.ts'
 import langHelper from './helpers/langHelper.ts'
 import styles from './style.scss?inline'
+import { DOCUMENTS_PUBLISHER, FAVORIS_MEDIACENTRE, FAVORIS_PORTAIL } from './utils/constants.ts'
 import { setLocale } from './utils/localizationUtils.ts'
+import { getToken } from './utils/soffitUtils.ts'
 import 'widget/dist/r-widget.js'
 
 const tagName = componentName(name)
@@ -36,15 +41,94 @@ export class ReciaWidgetsWrapper extends LitElement {
     setLocale(lang)
     langHelper.setLocale(lang)
     updateWhenLocaleChanges(this)
+    this.widgetKeyArray.push(FAVORIS_PORTAIL)
+    this.widgetKeyArray.push(FAVORIS_MEDIACENTRE)
+    this.widgetKeyArray.push(DOCUMENTS_PUBLISHER)
+
+    addEventListener('DOMContentLoaded', async () => {
+      const soffit: string = await getToken(import.meta.env.VITE_USER_RIGHTS_URI)
+      for (const value of this.widgetKeyArray) {
+        this.buildWidget(value, soffit)
+      }
+    })
   }
 
-  render(): TemplateResult {
+  widgetKeyArray: Array<string> = []
+
+  async buildWidget(key: string, soffit: string) {
+    let itemsAsString: string = await this.getWidgetData(key, soffit)
+    itemsAsString = this.itemsAsStringLinksProcessing(itemsAsString, key)
+
+    const widgetData: WidgetData = new WidgetData(this.tempGetName(key), '', '#key', '', false, itemsAsString)
+    this.widgetDataMap.set(key, widgetData)
+    this.requestUpdate()
+  }
+
+  async getWidgetData(key: string, soffit: string): Promise<string> {
+    switch (key) {
+      case FAVORIS_PORTAIL:
+        return await window.WidgetAdapter.getFavorisPortail()
+
+      case FAVORIS_MEDIACENTRE:
+        return await window.WidgetAdapter.getFavorisMediacentre(soffit)
+
+      case DOCUMENTS_PUBLISHER:
+        return await window.WidgetAdapter.getDocumentsPublisher(soffit)
+      default:
+        return ''
+    }
+  }
+
+  itemsAsStringLinksProcessing(itemsAsString: string, key: string): string {
+    switch (key) {
+      case FAVORIS_PORTAIL:
+      { const itemArray: Array<Item> = JSON.parse(itemsAsString)
+        const favorisLinkPattern: string = import.meta.env.VITE_FAVORIS_LINK_PATTERN
+        const itemArrayModified: Array<Item> = itemArray.map(v => new Item(v.name, favorisLinkPattern.replace('{fname}', v.link), v.icon))
+        return JSON.stringify(itemArrayModified)
+      }
+
+      default:
+        return itemsAsString
+    }
+  }
+
+  tempGetName(key: string): string {
+    switch (key) {
+      case FAVORIS_PORTAIL:
+        return 'Favoris'
+
+      case FAVORIS_MEDIACENTRE:
+        return 'Médiacentre'
+
+      case DOCUMENTS_PUBLISHER:
+        return 'Documents'
+
+      default:
+        console.error(`clé inconnue ${key}`)
+        return ''
+    }
+  }
+
+  @state()
+  widgetDataMap: Map<string, WidgetData> = new Map()
+
+  getWidgetRender(key: string): TemplateResult {
+    if (this.widgetDataMap.has(key)) {
+      const widgetData: WidgetData = this.widgetDataMap.get(key)
+      return html`
+      <r-widget
+        role="listitem"
+        name="${widgetData.name}"
+        link="${widgetData.link}"
+        empty-text="${widgetData.emptyText}"
+        items=${widgetData.items}
+        >
+      </r-widget>
+    `
+    }
+    // valeur par défaut temporaire
     return html`
-      <div class="widget">
-        <header>
-          <h2 class="sr-only">Accès rapides</h2>
-        </header>
-        <ul class="widget-tiles">
           <r-widget
             role="listitem"
             name="Favoris"
@@ -68,23 +152,23 @@ export class ReciaWidgetsWrapper extends LitElement {
               }
             ]'
           >
-          </r-widget>
-          <r-widget
-            role="listitem"
-            name="Médiacentre"
-            subtitle="Consulté récement"
-            link="/portail/p/Mediacentre"
-            empty-text="Aucun favoris"
-            empty-discover
-          >
-          </r-widget>
-          <r-widget
-            role="listitem"
-            name="Documents"
-            link="#3"
-            empty-text="Aucun document"
-          >
-          </r-widget>
+          </r-widget>`
+  }
+
+  render(): TemplateResult {
+    return html`
+      <div class="widget">
+        <header>
+          <h2 class="sr-only">Accès rapides</h2>
+        </header>
+        <ul class="widget-tiles">
+          ${repeat(
+            this.widgetKeyArray,
+            (widgetKey: string) => widgetKey,
+            (widgetKey: string) => html`
+            ${this.getWidgetRender(widgetKey)}
+            `,
+          )}
         </ul>
       </div>
     `
