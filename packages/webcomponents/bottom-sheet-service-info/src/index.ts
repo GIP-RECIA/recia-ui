ad3c3416dd97457c8e8360cde9e4f6a6681d3311
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-06-19 15:25:02 +0200

    feat(bottom-sheet-service-info): add project skeleton

---
+++ packages/webcomponents/bottom-sheet-service-info/src/index.ts
@@ -0,0 +1,99 @@
+/**
+ * Copyright (C) 2025 GIP-RECIA, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { ReciaBottomSheet } from 'bottom-sheet/src'
+import type { TemplateResult } from 'lit'
+import type { Ref } from 'lit/directives/ref.js'
+import { localized, updateWhenLocaleChanges } from '@lit/localize'
+import { css, html, LitElement, unsafeCSS } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import { createRef, ref } from 'lit/directives/ref.js'
+import { componentName } from '../../common/config.ts'
+import { name } from '../package.json'
+import langHelper from './helpers/langHelper.ts'
+import styles from './style.scss?inline'
+import { setLocale } from './utils/localizationUtils.ts'
+import 'bottom-sheet/dist/r-bottom-sheet.js'
+import 'service-info-layout/dist/r-service-info-layout.js'
+
+const tagName = componentName(name)
+
+@localized()
+@customElement(tagName)
+export class ReciaBottomSheetServiceInfo extends LitElement {
+  @property({ type: String, attribute: 'portal-info-api-url' })
+  portalInfoApiUrl?: string
+
+  @property({ type: String, attribute: 'service-info-api-url' })
+  serviceInfoApiUrl?: string
+
+  private bottomSheetRef: Ref<ReciaBottomSheet> = createRef()
+
+  constructor() {
+    super()
+    const lang = langHelper.getPageLang()
+    setLocale(lang)
+    langHelper.setLocale(lang)
+    updateWhenLocaleChanges(this)
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback()
+    this.addEventListener('service-info', this.handleEvent.bind(this))
+  }
+
+  disconnectedCallback(): void {
+    super.disconnectedCallback()
+    this.removeEventListener('service-info', this.handleEvent.bind(this))
+  }
+
+  open(): void {
+    this.bottomSheetRef.value!.open()
+  }
+
+  close(): void {
+    this.bottomSheetRef.value!.close()
+  }
+
+  handleEvent(e: Event): void {
+    const event = e as CustomEvent
+    if (!event.detail.fname)
+      return
+
+    this.open()
+  }
+
+  render(): TemplateResult {
+    return html`
+      <r-bottom-sheet
+        ${ref(this.bottomSheetRef)}
+      >
+        <r-service-info-layout
+          @close="${() => this.close()}"
+        >
+        </r-service-info-layout>
+      </r-bottom-sheet>
+    `
+  }
+
+  static styles = css`${unsafeCSS(styles)}`
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [tagName]: ReciaBottomSheetServiceInfo
+  }
+}
