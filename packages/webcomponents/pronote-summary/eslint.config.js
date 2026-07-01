728c84d5442c127d8f7b58772e0b4d20f8ea51f1
Ewann ROUX <ewann.roux@recia.fr>
2026-07-01 10:29:40 +0200

    chore(pronote-summary): init

---
+++ packages/webcomponents/pronote-summary/eslint.config.js
@@ -0,0 +1,17 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  typescript: true,
+  formatters: {
+    css: true,
+    html: true,
+    prettierOptions: {
+      singleAttributePerLine: true,
+    },
+  },
+  ignores: [
+    './src/generated/locales',
+    './src/generated/locale-codes.ts',
+    './public/widgets-adapter/*.js',
+  ],
+})
