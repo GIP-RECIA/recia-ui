a06ee6335a6a123a62a95fe2f7d6c73634c2356b
Ewann ROUX <ewann.roux@recia.fr>
2026-08-07 14:37:25 +0200

    chore(projet-pronote): initial commit

---
+++ packages/webcomponents/projet-pronote/README.md
@@ -0,0 +1,10 @@
+# r-projet-pronote
+
+- [r-projet-pronote](#r-projet-pronote)
+  - [Propriétés](#propriétés)
+
+## Propriétés
+
+| Nom | Type  | Obligatoire | Default | Description |
+| --- | :---: | :---------: | :-----: | ----------- |
+| ``  |  ``   |     ``      |   ``    |             |
---
+++ packages/webcomponents/projet-pronote/src/index.ts
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2025 GIP-RECIA, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { TemplateResult } from 'lit'
+import { localized } from '@lit/localize'
+import { componentName } from 'common/config.ts'
+import { css, html, LitElement, unsafeCSS } from 'lit'
+import { name } from '../package.json'
+import langHelper from './helpers/langHelper.ts'
+import styles from './style.scss?inline'
+import { setLocale } from './utils/localizationUtils.ts'
+
+@localized()
+export class ReciaProjetPronote extends LitElement {
+  constructor() {
+    super()
+    const lang = langHelper.getPageLang()
+    setLocale(lang)
+    langHelper.setLocale(lang)
+  }
+
+  render(): TemplateResult {
+    return html`
+      <div class="">
+      </div>
+    `
+  }
+
+  static styles = css`${unsafeCSS(styles)}`
+}
+
+const tagName = componentName(name)
+
+if (!customElements.get(tagName)) {
+  customElements.define(tagName, ReciaProjetPronote)
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [tagName]: ReciaProjetPronote
+  }
+}
